6017627fa4d5c4ee0497726066f5c3f3ce348f85
Jordi_i7 <jordi.z80@gmail.com>
2024-05-07 14:04:40 +0200

    Initial commit

---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ServicesWithAttributes
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddAutoRegisterServices (this IServiceCollection services)
+		{
+			var assemblies = AppDomain.CurrentDomain.GetAssemblies ();
+			foreach (var assembly in assemblies)
+			{
+				var typesWithAttributes = assembly.DefinedTypes
+					.Where (type => type.GetCustomAttributes (typeof (ServiceAttribute), true).Any ())
+					.ToList ();
+
+				foreach (var type in typesWithAttributes)
+				{
+					var attributeList = type.GetCustomAttributes<ServiceAttribute> ();
+
+					foreach (var attribute in attributeList)
+					{
+						var lifetime = attribute.Lifetime;
+
+						Type baseType = type.AsType ();
+
+						Type serviceType = attribute.ServiceInterface;
+						if (serviceType == null) serviceType = baseType;
+
+						switch (lifetime)
+						{
+							case ServiceLifetime.Singleton:
+								services.AddSingleton (serviceType, baseType);
+								break;
+							case ServiceLifetime.Scoped:
+								services.AddScoped (serviceType, baseType);
+								break;
+							case ServiceLifetime.Transient:
+								services.AddTransient (serviceType, baseType);
+								break;
+						}
+					}
+				}
+			}
+
+			return services;
+		}
+	}
+}
+
---
+++ ServiceRegisterAttributes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+//=============================================================================
+/// <summary></summary>
+[AttributeUsage (AttributeTargets.Class, Inherited = false)]
+public class ServiceAttribute : Attribute
+{
+	public ServiceLifetime Lifetime { get; }
+	public Type? ServiceInterface { get; }
+
+	public ServiceAttribute (ServiceLifetime lifetime)
+	{
+		Lifetime = lifetime;
+	}
+
+	public ServiceAttribute (ServiceLifetime lifetime, Type interfaceType)
+	{
+		Lifetime = lifetime;
+		ServiceInterface = interfaceType;
+	}
+}
+
